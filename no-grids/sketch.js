f32813646846e7bdc1df310bc72a2d29045860b6
stephenkistner <skistner@mica.edu>
2016-02-26 08:52:18 -0500

    Added files via upload

--- no-grids/sketch.js
+++ no-grids/sketch.js
@@ -17,6 +17,8 @@ var line1Offset,line2Offset;
 
 var calibre;
 
+var wiggleL;
+
 function preload() {
   imgN = loadImage("assets/N.png");
   imgO = loadImage("assets/O.png");
@@ -42,14 +44,41 @@ function draw() {
   fill(0,9);
   rect(0,0,width,height);
 scaler=width*height/5000000;
-   image(imgO, startX+1000*scaler, startY,imgO.width*scaler,imgO.height*scaler);
-  image(imgG, startX, startY+1500*scaler,imgG.width*scaler,imgG.height*scaler);
-  image(imgR, startX+1000*scaler, startY+800*scaler,imgR.width*scaler,imgR.height*scaler);
-    image(imgS, startX+4.2*1000*scaler, startY+800*scaler,imgS.width*scaler,imgS.height*scaler);
-  image(imgN, startX, startY,imgN.width*scaler,imgN.height*scaler);
- 
-  image(imgI, startX+2*1000*scaler, startY+800*scaler,imgI.width*scaler,imgI.height*scaler);
-  image(imgD, startX+2.7*1000*scaler, startY+800*scaler,imgD.width*scaler,imgD.height*scaler);
+wiggleX = noise(t);
+wiggleX = map(wiggleX,0,1,0,100);
+wiggleY = noise(t,10);
+wiggleY = map(wiggleY,0,1,0,100);
+  image(imgN, startX+wiggleX, startY+wiggleY,imgN.width*scaler,imgN.height*scaler);
+wiggleX = noise(t,11);
+wiggleX = map(wiggleX,0,1,0,100);
+wiggleY = noise(t,12);
+wiggleY = map(wiggleY,0,1,0,100);
+   image(imgO, startX+1000*scaler+wiggleX, startY+wiggleY,imgO.width*scaler,imgO.height*scaler);
+wiggleX = noise(t,13);
+wiggleX = map(wiggleX,0,1,0,100);
+wiggleY = noise(t,14);
+wiggleY = map(wiggleY,0,1,0,100);
+  image(imgG, startX+wiggleX, startY+1500*scaler+wiggleY,imgG.width*scaler,imgG.height*scaler);
+wiggleX = noise(t,15);
+wiggleX = map(wiggleX,0,1,0,100);
+wiggleY = noise(t,16);
+wiggleY = map(wiggleY,0,1,0,100);
+  image(imgR, startX+1000*scaler+wiggleX, startY+800*scaler+wiggleY,imgR.width*scaler,imgR.height*scaler);
+wiggleX = noise(t,17);
+wiggleX = map(wiggleX,0,1,0,100);
+wiggleY = noise(t,18);
+wiggleY = map(wiggleY,0,1,0,100);
+  image(imgS, startX+4.2*1000*scaler+wiggleX, startY+800*scaler+wiggleY,imgS.width*scaler,imgS.height*scaler);
+wiggleX = noise(t,19);
+wiggleX = map(wiggleX,0,1,0,100);
+wiggleY = noise(t,20);
+wiggleY = map(wiggleY,0,1,0,100);
+  image(imgI, startX+2*1000*scaler+wiggleX, startY+800*scaler+wiggleY,imgI.width*scaler,imgI.height*scaler);
+wiggleX = noise(t,21);
+wiggleX = map(wiggleX,0,1,0,100);
+wiggleY = noise(t,22);
+wiggleY = map(wiggleY,0,1,0,100);
+  image(imgD, startX+2.7*1000*scaler+wiggleX, startY+800*scaler+wiggleY,imgD.width*scaler,imgD.height*scaler);
 
   
   noStroke();
@@ -77,9 +106,9 @@ scaler=width*height/5000000;
     line2Offset = -45;
 
 
-  for (var i=-10000; i<10000; i+=200) {
-  line(wiggleX-textWidth(string1)/2+2*i,wiggleY+line1Offset+i,wiggleX1+textWidth(string2)/2+2*i,wiggleY1+line2Offset+i);
-}
+
+  line(wiggleX-textWidth(string1)/2,wiggleY+line1Offset,wiggleX1+textWidth(string2)/2,wiggleY1+line2Offset);
+
   t+=0.002;
   
   
